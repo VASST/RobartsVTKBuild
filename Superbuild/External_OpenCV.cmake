bbb0371748d34cceb08bc559bcc26eb91e15afc8
Adam Rankin <arankin@vasstbuild-linux.robarts.ca>
2018-03-07 18:43:02 -0500

    Updating opencv version to enable cuda 9 build

--- Superbuild/External_OpenCV.cmake
+++ Superbuild/External_OpenCV.cmake
@@ -2,7 +2,7 @@ IF(OpenCV_DIR)
   FIND_PACKAGE(OpenCV 3.2.0 REQUIRED NO_MODULE PATHS ${OpenCV_DIR})
 
   MESSAGE(STATUS "Using OpenCV available at: ${OpenCV_DIR}")
-  
+
   SET(RobartsVTK_OpenCV_DIR ${OpenCV_DIR} CACHE Internal "Path to OpenCV contents.")
 ELSE()
   MESSAGE(STATUS "Downloading and building OpenCV from: https://github.com/opencv/opencv.git")
@@ -13,8 +13,8 @@ ELSE()
   IF(NOT CUDA_FOUND)
     LIST(APPEND EXTRA_OPENCV_ARGS -DWITH_CUDA:BOOL=OFF)
   ELSE()
-    LIST(APPEND EXTRA_OPENCV_ARGS 
-      -DWITH_CUDA:BOOL=ON 
+    LIST(APPEND EXTRA_OPENCV_ARGS
+      -DWITH_CUDA:BOOL=ON
       -DCUDA_GENERATION:STRING=${CUDA_GENERATION}
       -DBUILD_opencv_cudalegacy:BOOL=OFF
       -DCUDA_TOOLKIT_ROOT_DIR:PATH=${CUDA_TOOLKIT_ROOT_DIR})
@@ -32,9 +32,9 @@ ELSE()
       -DQt5OpenGL_DIR:PATH=${Qt5OpenGL_DIR}
       -DQt5Test_DIR:PATH=${Qt5Test_DIR})
   ENDIF()
-  
+
   LIST(APPEND EXTRA_OPENCV_ARGS -DBUILD_opencv_python2:BOOL=OFF)
-  
+
   IF( ${CMAKE_GENERATOR} MATCHES "Visual Studio 11" )
     SET(ep_common_cxx_flags "${ep_common_cxx_flags} /D_VARIADIC_MAX=10")
   ENDIF()
@@ -47,9 +47,9 @@ ELSE()
     BINARY_DIR "${RobartsVTK_OpenCV_DIR}"
     #--Download step--------------
     GIT_REPOSITORY https://github.com/opencv/opencv.git
-    GIT_TAG 3.2.0
+    GIT_TAG 8edc2e5aaffa8c349ff9184ae77b3cccdea7cf6e
     #--Configure step-------------
-    CMAKE_ARGS 
+    CMAKE_ARGS
       ${ep_common_args}
       -DCMAKE_C_FLAGS=${ep_common_c_flags}
       -DCMAKE_CXX_FLAGS=${ep_common_cxx_flags}
@@ -61,7 +61,7 @@ ELSE()
       -DCMAKE_LIBRARY_OUTPUT_DIRECTORY:PATH=${CMAKE_LIBRARY_OUTPUT_DIRECTORY}
       -DCMAKE_ARCHIVE_OUTPUT_DIRECTORY:PATH=${CMAKE_ARCHIVE_OUTPUT_DIRECTORY}
       -DBUILD_DOCS:BOOL=OFF
-      -DVTK_DIR:PATH=${RobartsVTK_VTK_DIR} 
+      -DVTK_DIR:PATH=${RobartsVTK_VTK_DIR}
       -DWITH_OPENGL:BOOL=ON
       -DWITH_VFW:BOOL=OFF
       -DWITH_MSMF:BOOL=ON
